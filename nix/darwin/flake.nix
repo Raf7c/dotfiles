240406bebcac8d32c0a0afd86e89299d5946fc11
Raphael C <122828688+Raf7c@users.noreply.github.com>
2024-12-10 00:07:55 +0100

    feat: add home-manger to flake

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -6,11 +6,16 @@
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }:
   let
-    configuration = { pkgs, config, ... }: {
+    configuration = { pkgs, lib, config, ... }: {
 
       nixpkgs.config.allowUnfree = true;
 
@@ -21,7 +26,6 @@
 	  pkgs.kitty
 	  pkgs.alacritty
           pkgs.neovim
-          pkgs.obsidian
           pkgs.tmux
         ];
 
@@ -33,9 +37,12 @@
         casks = [
           "arc"
           "firefox"
+	  "raycast"
+	  "obsidian"
+	  "discord"
         ];
         masApps = {
-          "CleanMyMac" = 1339170533;
+	  "ZSA Keymapp" = 6472865291;
         };
         onActivation.cleanup = "zap";
       };
@@ -62,38 +69,36 @@
 
       system.defaults = {
         dock.autohide  = true;
+	dock.autohide-time-modifier = 0.4;
 	finder.FXPreferredViewStyle = "clmv";
 	loginwindow.GuestEnabled  = false;
 	NSGlobalDomain.AppleICUForce24HourTime = true;
+	NSGlobalDomain.KeyRepeat = 1;
+	NSGlobalDomain.AppleShowAllExtensions = true;
+	finder.AppleShowAllFiles = true;
+	dock.mru-spaces = false;
+	screencapture.location = "~/Pictures/screenshots";
+	screensaver.askForPasswordDelay = 10;
       };
 
-      # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
       programs.zsh.enable = true;  # default shell on catalina
       # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
       system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+      security.pam.enableSudoTouchIdAuth = true;
     };
   in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
-      modules = [configuration];
+      modules = [
+        configuration
+	home-manager.darwinModules.home-manager 
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
