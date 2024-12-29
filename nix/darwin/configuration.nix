7023ad61c0ec920e4144510805dc5bff1d90fb27
Raphael C <122828688+Raf7c@users.noreply.github.com>
2024-12-29 03:16:32 +0100

    feat: default system

--- nix/darwin/configuration.nix
+++ nix/darwin/configuration.nix
@@ -26,4 +26,32 @@
   system.stateVersion = 5;
   nixpkgs.hostPlatform = "aarch64-darwin";
   security.pam.enableSudoTouchIdAuth = true;
+  
+  system.defaults = {
+    dock = {
+      autohide = true;
+      autohide-time-modifier = 0.1;
+      autohide-delay = 0.0;
+      mru-spaces = false;
+      show-recents = false;
+    };
+    finder = {
+      FXPreferredViewStyle = "clmv";
+      AppleShowAllFiles = true;
+    };
+    NSGlobalDomain = {
+      AppleICUForce24HourTime = true;
+      AppleShowAllExtensions = true;
+      NSAutomaticSpellingCorrectionEnabled = false;
+    };
+    screencapture = {
+      location = "~/Pictures/screenshots";
+      type = "png";
+    };
+    screensaver = {
+      askForPassword = true;
+      askForPasswordDelay = 10;
+    };
+  };
+
 }
