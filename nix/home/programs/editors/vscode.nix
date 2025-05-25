cc8438bc87b86a2e64119b4701f32af1c38a8ee1
Raphael C <122828688+Raf7c@users.noreply.github.com>
2025-05-25 22:48:44 +0200

    fix: Path Home

--- nix/home/programs/editors/vscode.nix
+++ nix/home/programs/editors/vscode.nix
@@ -3,15 +3,19 @@
 {
   home.file = {
     "Library/Application Support/Code/User/settings.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/nix/home/common/core/vscode/settings.json";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/nix/home/programs/editors/vscode/settings.json";
       onChange = ''
-        cp "$HOME/Library/Application Support/Code/User/settings.json" "${config.home.homeDirectory}/.dotfiles/nix/home/programs/editors/vscode/settings.json"
+        if [ -f "$HOME/Library/Application Support/Code/User/settings.json" ]; then
+          cp "$HOME/Library/Application Support/Code/User/settings.json" "${config.home.homeDirectory}/.dotfiles/nix/home/programs/editors/vscode/settings.json"
+        fi
       '';
     };
     "Library/Application Support/Code/User/keybindings.json" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/nix/home/programs/editors/vscode/keybindings.json";
       onChange = ''
-        cp "$HOME/Library/Application Support/Code/User/keybindings.json" "${config.home.homeDirectory}/.dotfiles/nix/home/programs/editors/vscode/keybindings.json"
+        if [ -f "$HOME/Library/Application Support/Code/User/keybindings.json" ]; then
+          cp "$HOME/Library/Application Support/Code/User/keybindings.json" "${config.home.homeDirectory}/.dotfiles/nix/home/programs/editors/vscode/keybindings.json"
+        fi
       '';
     };
   };
