784e12570775fe85201140261df1fd7b9dca1234
Raphaël C <122828688+Raf7c@users.noreply.github.com>
2025-10-25 22:31:14 +0200

    feat: tmux config

--- install/link_global.sh
+++ install/link_global.sh
@@ -7,23 +7,26 @@ set -eu
 
 echo "🔗 Creating symbolic links..."
 
-# Create .config directory
+# Create directories
 mkdir -p "$HOME/.config"
 mkdir -p "$HOME/.local/bin"
 
-# .config links
+# Config links
 ln -sfn "$HOME/.dotfiles/.config/kitty"		"$HOME/.config/kitty"
 ln -sfn "$HOME/.dotfiles/.config/git"		"$HOME/.config/git"
 ln -sfn "$HOME/.dotfiles/.config/starship.toml" "$HOME/.config/starship.toml"
 ln -sfn "$HOME/.dotfiles/.config/ghostty"   "$HOME/.config/ghostty"
 ln -sfn "$HOME/.dotfiles/.config/shell" "$HOME/.config/shell"
 ln -sfn "$HOME/.dotfiles/.config/bat" "$HOME/.config/bat"
+ln -sfn "$HOME/.dotfiles/.config/tmux" "$HOME/.config/tmux"
 
-# Shell links
+# Login shell links
 ln -sfn "$HOME/.dotfiles/zsh/.zshenv" "$HOME/.zshenv"
 ln -sfn "$HOME/.dotfiles/zsh/.zprofile" "$HOME/.zprofile"
 ln -sfn "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.zshrc"
 ln -sfn "$HOME/.dotfiles/bash/.bashrc" "$HOME/.bashrc"
+
+# asdf links
 ln -sfn "$HOME/.dotfiles/.tool-versions" "$HOME/.tool-versions"
 
 # Vim links
