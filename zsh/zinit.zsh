22681dfb85e1a7fd3718c43400ab17b7829e115b
Raphaël C <122828688+Raf7c@users.noreply.github.com>
2025-10-27 16:29:09 +0100

    feat/perf: optimize shell performance and add modern CLI tools

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -3,10 +3,17 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d "$ZINIT_HOME/.git" ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "$ZINIT_HOME/zinit.zsh"
 
-# Load all plugins
+# Load critical plugin first
 zinit load zsh-users/zsh-completions
-zinit load zsh-users/zsh-autosuggestions
-zinit load zsh-users/zsh-syntax-highlighting
+
+# Load non-critical plugins asynchronously
+zinit wait lucid for \
+    atload"_zsh_autosuggest_start" \
+        zsh-users/zsh-autosuggestions \
+    atinit"zicompinit; zicdreplay" \
+        zsh-users/zsh-syntax-highlighting
+
+# Load fzf-tab normally
 zinit light Aloxaf/fzf-tab
 
 # Initialize completions
@@ -19,8 +26,8 @@ compinit -C -d "$XDG_CACHE_HOME/zsh/.zcompdump"
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always --icons $realpath 2>/dev/null || ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -1 --color=always --icons $realpath 2>/dev/null || ls --color $realpath'
 
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
