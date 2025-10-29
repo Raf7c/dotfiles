2ca4ec00fa9a83b3ea679e1722970aadd2785036
Raphaël C <122828688+Raf7c@users.noreply.github.com>
2025-10-29 02:43:03 +0100

    fix: load utils

--- install/macOS/packages.sh
+++ install/macOS/packages.sh
@@ -9,6 +9,9 @@ set -eu
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 PACKAGES_DIR="$SCRIPT_DIR/../packages"
 
+# Load utilities
+. "$SCRIPT_DIR/../lib/utils.sh"
+
 # Load unified package installer
 . "$SCRIPT_DIR/../lib/install_packages.sh"
 
